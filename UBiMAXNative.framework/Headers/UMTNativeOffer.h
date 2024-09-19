9d9ba0b17d74250aa5420d904fb46609a66b3a39
guoqiang <guoqiang.zhu@ubixai.com>
2024-09-19 22:52:47 +0800

    add UBiMAXNative 1.0.0

---
+++ UBiMAXNative.framework/Headers/UMTNativeOffer.h
@@ -0,0 +1,67 @@
+//
+//  UMTNativeOffer.h
+//  UMTNative
+//
+//  Created by guoqiang on 2024/8/27.
+//
+
+#import <UBiMAXAdSDK/UMTAdSDK.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@class UMTNative;
+@class UMTNativeOffer;
+
+@protocol UMTNativeDelegate <UMTAdDelegate>
+//// 信息流展示
+- (void)umtNativeAd:(UMTNative *)native didShowSucc:(UMTNativeOffer *)offer;
+//// 信息流渲染成功，仅模板广告，可能不回调
+- (void)umtNativeAd:(UMTNative *)native didAdViewRenderSucc:(UMTNativeOffer *)offer;
+/// 信息流渲染失败，仅模板广告，可能不回调
+- (void)umtNativeAd:(UMTNative *)native didAdView:(UMTNativeOffer *)offer renderFailed:(UMTError *)error;
+/// 信息流展示失败，仅支持 baidu
+- (void)umtNativeAd:(UMTNative *)native didShow:(UMTNativeOffer *)offer failed:(UMTError *)error;
+/// 信息流点击
+- (void)umtNativeAd:(UMTNative *)native didClick:(UMTNativeOffer *)offer;
+/// 信息流关闭
+- (void)umtNativeAd:(UMTNative *)native didClose:(UMTNativeOffer *)offer;
+/// 信息流详情关闭，
+- (void)umtNativeAd:(UMTNative *)native didDetailClose:(UMTNativeOffer *)offer;
+/// 信息流，视频播放开始
+- (void)umtNativeAd:(UMTNative *)native offer:(UMTNativeOffer *)offer didPlayStartExtra:(NSDictionary *)extra;
+/// 信息流，视频播放状态变更
+- (void)umtNativeAd:(UMTNative *)native offer:(UMTNativeOffer *)offer didChangedPlayStatus:(UMTVideoPlayerStatus)status;
+/// 信息流，视频播放完成或异常
+- (void)umtNativeAd:(UMTNative *)native offer:(UMTNativeOffer *)offer didPlayFinishExtra:(NSDictionary *)extra failed:(UMTError *)error;
+
+@end
+
+@interface UMTNativeOffer : UMTInterfaceBaseObject
+//// 信息流关联对象
+@property (nonatomic, weak) UMTNative *nativeAd;
+/// 是否模板广告
+@property (nonatomic, assign) BOOL isExpress;
+/// 如果是模板广告可直接将该视图添加到需要展示的位置，如果是非模板广告可对该视图内部控件进行布局操作
+@property (nonatomic, strong) UMTNativeAdView *canvasView;
+//// 信息流交互代理对象
+@property (nonatomic, weak) id<UMTNativeDelegate> delegate;
+///
+@property (nonatomic, weak) UIViewController *rootViewController;
+
+/// [必要]渲染广告，模板广告开发者无需传入自渲染容器 selfRenderView
+- (void)renderAdView:(UMTNativeAdView *)adView selfRenderView:(UIView *)view;
+
+/**
+ 注册自渲染广告点击事件.
+ @param containerView : 必要.
+                        自渲染广告容器.
+ @param clickableViews :  点击组件数组
+ @param closableViews :  点击关闭组件数组.
+ */
+- (void)registerContainer:(__kindof UIView *)containerView
+       withClickableViews:(NSArray<__kindof UIView *> *_Nullable)clickableViews
+            closableViews:(NSArray<__kindof UIView *> *_Nullable)closableViews;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
